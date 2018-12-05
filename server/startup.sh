bdb700df77648579dc1937a855bce2017f917e91
rchari66 <42802355+rchari66@users.noreply.github.com>
2018-12-05 17:25:38 +0530

    Update startup.sh

--- server/startup.sh
+++ server/startup.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+# Download vdockPlugin for adding custom snippets
+git clone https://github.com/rchari66/vdock-plugin-cloud9.git ${C9_PATH}vdockPlugin
+cp -r cp -r ${C9_PATH}vdockPlugin /root/.c9/plugins/
+
 # start the cloud9
 node /work/c9sdk/server.js -p 8282 -w ${C9_PATH} >> /dev/null &
 
