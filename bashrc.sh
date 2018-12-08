a3f30e7edaa94f76fc0d1246c4bbfd4ecb495170
rchari66 <raghav>
2018-12-08 08:28:47 +0000

    git alias cmds added

--- bashrc.sh
+++ bashrc.sh
@@ -5,3 +5,16 @@ sourceUpdate() {
 
 # source all *.sh files in $SERVER_PATH/scripts
 sourceUpdate
+
+# Alias for git commands
+gitaliassetup() {
+	alias gd='git diff'
+	alias gb='git branch --sort=creatordate'
+	alias gcm="git checkout master"
+	alias gp='git pull'
+	alias gc='git checkout $1'
+	alias gdc='git diff --color-words'
+	alias gds='git diff --staged --color-words'  
+	alias gl='git log --oneline -n 20'
+	alias gs='git status'
+}
